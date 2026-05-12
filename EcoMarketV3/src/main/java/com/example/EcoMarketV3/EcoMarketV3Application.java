ad80080efef2a2c94b7458b1312e9bd452b85a65
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-12 12:28:00 -0400

    Add files via upload

---
+++ EcoMarketV3/src/main/java/com/example/EcoMarketV3/EcoMarketV3Application.java
@@ -0,0 +1,13 @@
+package com.example.EcoMarketV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMarketV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMarketV3Application.class, args);
+	}
+
+}
---
+++ EcoMarketV3/src/test/java/com/example/EcoMarketV3/EcoMarketV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoMarketV3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
