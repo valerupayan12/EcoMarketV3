70eb5a55ea575668ecdc08f8890efd85a1917dd8
belenvelasco <bel.velasco@duocuc.cl>
2026-05-14 11:11:19 -0400

    MicroServicioTiendaUbicacionCompleto

---
+++ MicroTiendaUbicacionV2/MicroTiendaUbicacionV2/src/main/java/com/example/MicroTiendaUbicacionV2/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.MicroTiendaUbicacionV2.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
